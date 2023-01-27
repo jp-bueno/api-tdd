dfe92212889aa0bb75fd22911db33be39d0cc704
bueno <joaopedrobueno2910@gmail.com>
2023-01-26 22:46:57 -0300

    initial commit
    initial commit

---
+++ src/main/java/br/com/jpbueno/apitdd/ApiTddApplication.java
@@ -0,0 +1,13 @@
+package br.com.jpbueno.apitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jpbueno/apitdd/ApiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jpbueno.apitdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
