10f9d5aa9fe37c2135c6ffa966d9f8c438570abf
bueno <joaopedrobueno2910@gmail.com>
2023-01-30 21:57:35 -0300

    Endpoint findAll para User

--- src/main/java/br/com/jpbueno/apitdd/resources/UserResource.java
+++ src/main/java/br/com/jpbueno/apitdd/resources/UserResource.java
@@ -2,6 +2,7 @@ package br.com.jpbueno.apitdd.resources;
 
 import br.com.jpbueno.apitdd.domain.User;
 import br.com.jpbueno.apitdd.domain.dto.UserDTO;
+import br.com.jpbueno.apitdd.repositories.UserRepository;
 import br.com.jpbueno.apitdd.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -20,10 +24,19 @@ public class UserResource {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> userDTO = userService.findAll().stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(userDTO);
     }
 }
--- src/main/java/br/com/jpbueno/apitdd/services/UserService.java
+++ src/main/java/br/com/jpbueno/apitdd/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.jpbueno.apitdd.services;
 
 import br.com.jpbueno.apitdd.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
